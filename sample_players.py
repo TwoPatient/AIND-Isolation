65cfacf9536a5557a5b79bbedb45bdea661d98e1
Ben Carrier <ben.carrier@lineview.com>
2017-05-15 18:52:46 +0100

    added heuristic_analysis.pdf and edited sample_players to work against manual players

--- sample_players.py
+++ sample_players.py
@@ -7,6 +7,8 @@ own agent and example heuristic functions.
 """
 
 from random import randint
+from game_agent import (MinimaxPlayer, AlphaBetaPlayer, custom_score,
+                        custom_score_2, custom_score_3)
 
 
 def null_score(game, player):
@@ -256,34 +258,36 @@ if __name__ == "__main__":
     from isolation import Board
 
     # create an isolation board (by default 7x7)
-    player1 = RandomPlayer()
-    player2 = GreedyPlayer()
+    player1 = AlphaBetaPlayer()
+    player2 = HumanPlayer()
     game = Board(player1, player2)
 
-    # place player 1 on the board at row 2, column 3, then place player 2 on
-    # the board at row 0, column 5; display the resulting board state.  Note
-    # that the .apply_move() method changes the calling object in-place.
-    game.apply_move((2, 3))
-    game.apply_move((0, 5))
-    print(game.to_string())
+    movetimelimit = 30000
 
-    # players take turns moving on the board, so player1 should be next to move
-    assert(player1 == game.active_player)
+    # # place player 1 on the board at row 2, column 3, then place player 2 on
+    # # the board at row 0, column 5; display the resulting board state.  Note
+    # # that the .apply_move() method changes the calling object in-place.
+    # game.apply_move((2, 3))
+    # game.apply_move((0, 5))
+    # print(game.to_string())
 
-    # get a list of the legal moves available to the active player
-    print(game.get_legal_moves())
+    # # players take turns moving on the board, so player1 should be next to move
+    # assert(player1 == game.active_player)
 
-    # get a successor of the current state by making a copy of the board and
-    # applying a move. Notice that this does NOT change the calling object
-    # (unlike .apply_move()).
-    new_game = game.forecast_move((1, 1))
-    assert(new_game.to_string() != game.to_string())
-    print("\nOld state:\n{}".format(game.to_string()))
-    print("\nNew state:\n{}".format(new_game.to_string()))
+    # # get a list of the legal moves available to the active player
+    # print(game.get_legal_moves())
+
+    # # get a successor of the current state by making a copy of the board and
+    # # applying a move. Notice that this does NOT change the calling object
+    # # (unlike .apply_move()).
+    # new_game = game.forecast_move((1, 1))
+    # assert(new_game.to_string() != game.to_string())
+    # print("\nOld state:\n{}".format(game.to_string()))
+    # print("\nNew state:\n{}".format(new_game.to_string()))
 
     # play the remainder of the game automatically -- outcome can be "illegal
     # move", "timeout", or "forfeit"
-    winner, history, outcome = game.play()
+    winner, history, outcome = game.play(time_limit=movetimelimit)
     print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
     print(game.to_string())
     print("Move history:\n{!s}".format(history))
